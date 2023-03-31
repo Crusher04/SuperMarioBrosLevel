a1ece059125fb8dad410810363653d84c9a8bd3c
Abay402 <abayjit402@hotmail.com>
2023-03-31 05:44:44 -0400

    Player Moves Left and Right

---
+++ CupHeadLevel/Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 35f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+     
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+     
+    }
+
+
+}
