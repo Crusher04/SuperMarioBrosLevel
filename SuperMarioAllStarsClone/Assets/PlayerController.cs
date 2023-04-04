daac91012a66be2de8ddce731a821aae74d5902d
Crusher04 <35314898+Crusher04@users.noreply.github.com>
2023-04-03 20:59:55 -0400

    working on character movement to test scene

---
+++ SuperMarioAllStarsClone/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+
+    public float moveSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
