b22b47612459d29fbdc6c222a13bb355702f91c1
Crusher04 <35314898+Crusher04@users.noreply.github.com>
2023-04-05 00:45:11 -0400

    Small mario moving and animating left and right works

--- SuperMarioAllStarsClone/Assets/Scripts/PlayerController.cs
+++ SuperMarioAllStarsClone/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,19 @@ public class PlayerController : MonoBehaviour
 {
     private Vector2 moveInput;
     private Rigidbody2D rb;
+    private Animator anim;
 
     public float moveSpeed = 1.0f;
+
+    public Vector2 oldPos = Vector2.zero;
+
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        oldPos = transform.position;
     }
 
     // Update is called once per frame
@@ -25,10 +31,28 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+
+        if(oldPos.x < transform.position.x)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("moving", true);
+        }
+        else if (oldPos.x > transform.position.x)
+        {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("moving", true);
+        }
+        else
+        {
+            anim.SetBool("moving", false);
+            
+        }
+
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+        oldPos = transform.position;
     }
 }
