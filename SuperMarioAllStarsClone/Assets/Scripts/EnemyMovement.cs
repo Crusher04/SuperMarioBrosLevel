3cecaeb03e922a09931570bc4e89ff72f80b8d0d
Abay402 <abayjit402@hotmail.com>
2023-04-06 10:09:47 -0400

    Created Multiple Animations for Enemies

--- SuperMarioAllStarsClone/Assets/Scripts/EnemyMovement.cs
+++ SuperMarioAllStarsClone/Assets/Scripts/EnemyMovement.cs
@@ -7,17 +7,23 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private float moveSpeed = 5f;
+    public float moveSpeed = 1.5f;
     private Rigidbody2D rb;
-
+    private Vector3 localScale;
 
     public bool moveRight;
+
+    public bool isKoopa;
+    public bool isGoomba;
+    public bool isPiranha;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
 
-
+        localScale = transform.localScale;
         rb = GetComponent<Rigidbody2D>();
 
     }
@@ -25,18 +31,20 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (moveRight)
+        if (isKoopa || isGoomba)
         {
+            if (moveRight)
+            {
 
-            rb.velocity = new Vector2(moveSpeed, 0);
+                rb.velocity = new Vector2(moveSpeed, 0);
 
-        }
-        else
-        {
+            }
+            else
+            {
 
-            rb.velocity = new Vector2(-moveSpeed, 0);
+                rb.velocity = new Vector2(-moveSpeed, 0);
 
+            }
         }
 
     }
@@ -45,19 +53,23 @@ public class EnemyMovement : MonoBehaviour
     void OnTriggerEnter2D(Collider2D trigger)
     {
 
-        if (trigger.gameObject.CompareTag("Switch"))
+        if (isKoopa || isGoomba)
         {
-            if (moveRight)
+            if (trigger.gameObject.CompareTag("Switch"))
             {
+                if (moveRight)
+                {
+                    localScale.x *= -1;
+                    transform.localScale = localScale;
+                    moveRight = false;
+                }
+                else
+                {
+                    localScale.x *= -1;
+                    transform.localScale = localScale;
+                    moveRight = true;
 
-                moveRight = false;
-
-            }
-            else
-            {
-
-                moveRight = true;
-
+                }
             }
         }
 
