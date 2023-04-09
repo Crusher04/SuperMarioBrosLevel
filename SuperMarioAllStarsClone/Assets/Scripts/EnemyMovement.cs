985983c0e570917bc96abd8e5346cf2df5b91122
Abay402 <abayjit402@hotmail.com>
2023-04-09 08:09:35 -0400

    Movement for Flying Goomba and Koopa is Complete

--- SuperMarioAllStarsClone/Assets/Scripts/EnemyMovement.cs
+++ SuperMarioAllStarsClone/Assets/Scripts/EnemyMovement.cs
@@ -1,70 +1,156 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+
+    ///Initialize Variables
+
+    
+    //Variables for speed
     public float moveSpeed = 1.5f;
+    private float newSpeed;
+    public float jumpSpeed;
+
+    //Variables for the object
     private Rigidbody2D rb;
     private Vector3 localScale;
 
-    public bool moveRight;
+    //Variables for movement
+    private bool moveRight;
+    private bool isFlying;
+    private bool isJumping;
 
+    //Variables for enemy type
     public bool isKoopa;
     public bool isGoomba;
     public bool isPiranha;
+    public bool hasWings;
 
+    //Other Variables
+    private int jumpCount;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //Get the RigidBody and the scale of the enemy
         localScale = transform.localScale;
         rb = GetComponent<Rigidbody2D>();
 
+        //Set the newSpeed to equal moveSpeed at the start
+        if (moveRight)
+        {
+
+            newSpeed = moveSpeed;
+
+        }
+        else
+        {
+
+            newSpeed = -moveSpeed;
+
+        }
+
+        //Set these variables to 0
+        jumpCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isKoopa || isGoomba)
+        //IF the enemy does not have wings than have it move right and left
+        if (!hasWings)
         {
+
+            //Set enemy velocity 
+            rb.velocity = new Vector2(newSpeed, 0);
+
+            //Change velocity depending on if the enemy is moving right or left
             if (moveRight)
             {
 
-                rb.velocity = new Vector2(moveSpeed, 0);
+                newSpeed = moveSpeed;
 
             }
             else
             {
 
-                rb.velocity = new Vector2(-moveSpeed, 0);
+                newSpeed = -moveSpeed;
 
             }
         }
 
+        //Check to see if the enemy is flying
+        if (isFlying == false)
+        {
+            //Check to see if the enemy has wings and if it is a Koopa
+            if(hasWings == true && isKoopa)
+            {
+                //Make the enemy fly     
+                rb.velocity = new Vector2(newSpeed / 2, jumpSpeed);
+                isFlying = true;
+            }
+
+
+            //Check to see if the enemy has wings and if it is a Goomba
+            //Goomba with wings has a different flying pattern than a Koopa with wings
+            if (hasWings == true && isGoomba)
+            {
+                
+                //Check to see if it is jumping 
+                if (isJumping == true)
+                {
+                    //Make the enemy jump
+                    rb.velocity = new Vector2(newSpeed / 2, jumpSpeed / 3);
+                    isJumping = false;
+                }
+                
+                //After 3 jumps make the enemy fly
+                if (jumpCount == 3)
+                {
+
+                    isJumping = false;
+                    rb.velocity = new Vector2(newSpeed / 2, jumpSpeed);
+                    isFlying = true;
+                    jumpCount = 0;
+
+                }
+            }
+
+
+        }        
+
     }
 
-
+    //Function that is called when enemy collides with something
     void OnTriggerEnter2D(Collider2D trigger)
     {
 
-        if (isKoopa || isGoomba)
+        //IF the enemy does not have wings
+        if (!hasWings)
         {
+            //Check to see if the enemy collided with a switch object
             if (trigger.gameObject.CompareTag("Switch"))
             {
+                //IF the enemy was moving right than make the enemy move left instead
                 if (moveRight)
                 {
+                    //Flip the enemy sprite when it changes directions
                     localScale.x *= -1;
                     transform.localScale = localScale;
                     moveRight = false;
                 }
+                //IF the enemy was moving left than make the enemy move right instead
                 else
                 {
+                    //Flip the enemy sprite when it changes directions
                     localScale.x *= -1;
                     transform.localScale = localScale;
                     moveRight = true;
@@ -72,6 +158,28 @@ public class EnemyMovement : MonoBehaviour
                 }
             }
         }
+
+        //IF the enemy does have wings
+        if (hasWings)
+        {
+            //Check to see if the enemy collided with an edge block or the ground
+            if (trigger.gameObject.CompareTag("Edge Block") || trigger.gameObject.CompareTag("Ground"))
+            {
+
+                //Set isFlying to false 
+                isFlying = false;
+                //IF isFlying is false than set isJumping to true
+                if (isFlying == false)
+                {
+                    isJumping = true;
+
+                }
+                //Increase Jump Count
+                jumpCount++;
+            }
+        }
+
+
 
     }
 
